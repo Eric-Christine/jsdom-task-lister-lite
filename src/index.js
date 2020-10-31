53da86003322411012828008aba1ee8919d08033
Eric Chrsitine <ericjchristine@gmail.com>
2020-10-30 18:17:05 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const submit_button = document.querySelector('#submit')
+  console.log(submit_button)
+
+  const submit_box = document.querySelector('#new-task-description')
+
+  const ul = document.querySelector('#tasks')
+
+
+  submit_button.addEventListener("click", function (e) {
+    e.preventDefault()
+    const new_list_item = document.createElement('li')
+    new_list_item.textContent = submit_box.value
+    ul.append(new_list_item)
+  })
 });
